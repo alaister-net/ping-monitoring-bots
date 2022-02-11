30a3ff176bbdebe97de4901d9c0b1d8f43af9277
Alaister Leung <contact@alaister.net>
2022-02-11 02:57:48 +0000

    Add node online status

--- __main__.py
+++ __main__.py
@@ -25,8 +25,7 @@ logger.addHandler(handler)
 client = disnake.Client()
 
 public_ch = private_ch = last_checked = status_msg = alert_msg = None
-checks = [None]*50
-alerted_checks = {}
+checks = [{}]*50
 
 @client.event
 async def on_ready():
@@ -52,11 +51,7 @@ async def on_message(msg):
     if name == 'MASTER':
         global checks
         diff = msg.created_at.replace(tzinfo=None) - last_checked
-        checks[int(i)] = {
-            'node': key,
-            'ping': int(diff.total_seconds() * 1000),
-            'latency': int(value),
-        }
+        update_data(index=int(i), node=key, online=True, delay=int(diff.total_seconds() * 1000), latency=int(value))
         
     elif key == 'MASTER':
         if value == 'PING':
@@ -83,52 +78,119 @@ async def on_message_delete(_):
         alert_msg = None
         return
 
+def update_data(index: int, node: str = None, online: bool = None, delay: int = None, latency: int = None, alerts: dict = None):
+    global checks
+    
+    if alerts is None:
+        if not checks[index]:
+            alerts = {
+                'delay': 0,
+                'latency': 0,
+            }
+        else:
+            alerts = checks[index]['alerts']
+    
+    checks[index] = {
+        'index': index,
+        'node': checks[index]['node'] if node is None else node,
+        'online': checks[index]['online'] if online is None else online,
+        'delay': checks[index]['delay'] if delay is None else delay,
+        'latency': checks[index]['latency'] if latency is None else latency,
+        'alerts': alerts,
+    }
+
 @tasks.loop(seconds=60.0)
 async def checker():
-    global last_checked, status_msg, alert_msg, alerted_checks
+    global last_checked, status_msg, alert_msg, checks
     
+    for check in checks:
+        if not check:
+            continue
+        
+        update_data(index=check['index'], online=False)
+        
     try:
         msg = await private_ch.send('0:MASTER:PING')
-        last_checked = msg.created_at.replace(tzinfo=None)
-        
+    except Exception as e:
+        print(e)
         await asyncio.sleep(5)
-        
-        embed = disnake.Embed(title='Node Status (for Discord bots)', color=disnake.Color.fuchsia(), timestamp=datetime.now())
-        embed.set_footer(text=f'Alaister.net Ping Monitoring Bot', icon_url='https://alaister.net/hotlink-ok/alaister_net_icon.png')
-        
-        view = disnake.ui.View(timeout=None)
-        view.add_item(disnake.ui.Button(label='System Status', url='https://status.alaister.net'))
-        view.add_item(disnake.ui.Button(label='GitHub', url='https://github.com/alaister-net/ping-monitoring-bots'))
-        
-        alerts = []
-        for check in checks:
-            if check is None:
-                continue
-            
-            embed.add_field(check['node'], f"`BOT PING` **{check['ping']}** ms \n`API LATENCY` **{check['latency']}** ms", inline=False)
-            
-            count = alerted_checks.get(check['node'], 0)
-            if check['ping'] >= 2000 or check['latency'] >= 1000:
-                alerted_checks[check['node']] = count + 1
-            elif count > 1:
-                alerted_checks[check['node']] = count - 1
-            else:
-                alerted_checks[check['node']] = 0
-        
-        status_msg = await public_ch.send(embed=embed, view=view) if status_msg is None else await status_msg.edit(embed=embed)
-        
-        alerts = []
-        for alerted_node, count in alerted_checks.items():
-            if count >= 3:
-                alerts.append(alerted_node)
-        
-        if alerts:
-            msg_body = f'<@&{admin_id}>, high ping/latency alert on {", ".join(alerts)} (<t:{int(time())}:R>)'
-            alert_msg = await public_ch.send(msg_body) if alert_msg is None else await alert_msg.edit(content=msg_body)
-    
-    except:
-        await asyncio.sleep(3)
         await checker()
+        return
+    
+    last_checked = msg.created_at.replace(tzinfo=None)
+    
+    await asyncio.sleep(10)
+    
+    embed = disnake.Embed(title='Node Status', color=disnake.Color.fuchsia(), timestamp=datetime.now())
+    embed.set_footer(text=f'Alaister.net Ping Monitoring Bot', icon_url='https://alaister.net/hotlink-ok/alaister_net_icon.png')
+    
+    view = disnake.ui.View(timeout=None)
+    view.add_item(disnake.ui.Button(label='System Status', url='https://status.alaister.net'))
+    view.add_item(disnake.ui.Button(label='GitHub', url='https://github.com/alaister-net/ping-monitoring-bots'))
+    
+    alerts = []
+    for check in checks:
+        if not check:
+            continue
+        
+        if check['online']:
+            status = ':green_circle: ONLINE'
+        else:
+            status = ':warning: DISCORD API ERROR'
+            alerts.append((check['node'], 'Discord API error'))
+        
+        embed.add_field(check['node'],
+        f"`STATUS` **{status}** \n`MESSAGE RESPONSE DELAY` **{check['delay']}** ms \n`DISCORD API LATENCY` **{check['latency']}** ms",
+        inline=False)
+        
+        counts = check.get('alerts', {
+            'delay': 0,
+            'latency': 0,
+        })
+        
+        if check['delay'] >= 2000:
+            counts['delay'] += 1
+        elif counts['delay'] > 2:
+            counts['delay'] -= 2
+        else:
+            counts['delay'] = 0
+        
+        if check['latency'] >= 1000:
+            counts['latency'] += 1
+        elif counts['latency'] > 2:
+            counts['latency'] -= 2
+        else:
+            counts['latency'] = 0
+        
+        update_data(index=check['index'], alerts=counts)
+        
+        if counts['delay'] >= 3:
+            alerts.append((check['node'], 'High message response delay'))
+        
+        if counts['latency'] >= 3:
+            alerts.append((check['node'], 'High Discord API latency'))
+    
+    try:
+        status_msg = await public_ch.send(embed=embed, view=view) if status_msg is None else await status_msg.edit(embed=embed)
+    except Exception as e:
+        print(e)
+        await asyncio.sleep(5)
+        await checker()
+        return
+    
+    if alerts:
+        msg_body = f'<@&{admin_id}>, **STATUS ALERTS:**'
+        
+        for alert in alerts:
+            msg_body += f'{alert[1]} alert on {alert[0]} (<t:{int(time())}:R>)'
+        
+        try:
+            alert_msg = await public_ch.send(msg_body) if alert_msg is None else await alert_msg.edit(content=msg_body)
+        except Exception as e:
+            print(e)
+            await asyncio.sleep(5)
+            await checker()
+            return
 
 @checker.before_loop
 async def before_check():
